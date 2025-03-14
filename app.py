56d17b65ea6c6432a31aeeb6ecaa545a9976604f
marianacamposss <mariana.m.campos@aluno.senai.br>
2025-03-14 12:46:40 -0300

    altera 02

--- app.py
+++ app.py
@@ -1,54 +1,32 @@
-from flask import Flask, request, render_template  
-import requests  
-import json  
+from flask import Flask, request, render_template, redirect
+import requests
 
-app = Flask(__name__)  
-
-API_ENDPOINT = 'https://randomuser.me/api/'  
+app = Flask(__name__)
 
 
+API = 'https://api.thecatapi.com/v1/images/search'
 
-# requisição à API e retorna os dados em formato JSON
-def obter_dados_api():
-    try:
-        resposta = requests.get(API_ENDPOINT)  # requisição GET para a API
-        resposta.raise_for_status() 
-        return resposta.json()  
-    except requests.exceptions.RequestException as erro:
-        return {"erro": f"Erro ao fazer a requisição para a API: {erro}"} 
-    except json.JSONDecodeError as erro:
-        return {"erro": f"Erro ao processar a resposta da API: {erro}"}  # Trata erros ao decodificar o JSON
-
-
-#extrai o link da imagem a partir dos dados
-def extrair_url_imagem(dados):
-    try:
-        return dados["results"][0]["picture"]["large"]  # Extrai o link da imagem
-    except (KeyError, IndexError):  # Trata possíveis erros se a estrutura dos dados mudar
-        return None  
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'GET': 
-        return render_template('index.html') 
+    return render_template('index.html')
 
-    nome = request.form.get('nome', None) 
-    if not nome:  # Se o nome não foi informado
-        return render_template('index.html', erro="⚠ Você precisa informar um nome!") 
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'GET':
+        return redirect('/')
 
-    dados = obter_dados_api()  # Faz a requisição pra obter os dados do usuário
+    nome = request.form.get('name', None)
+    if not nome:
+        return render_template('index.html', erro="Você não informou o seu nome!")
+    
+    response = requests.get(API)
 
-    if "erro" in dados:  # erro na requisição
-        return render_template('index.html', erro=dados["erro"]) 
-
-    url_imagem = extrair_url_imagem(dados)  # Extrai o URL da imagem do rosto do usuário
-
-    if url_imagem:  
-        return render_template('index.html', nome=nome, url_imagem=url_imagem)  # Exibe a imagem e o nome
-    else:  
-        return render_template('index.html', erro="Erro ao obter o URL da imagem.")  # Exibe erro
-
+    if response.status_code == 200:
+        data = response.json()
+        url = data[0]['url']
+        return render_template('index.html', nome=nome, url=url)
+    else:
+        return render_template('index.html', erro="Erro no sistema! Volte outra hora.")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
